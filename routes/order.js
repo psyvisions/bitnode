82747352eb7a14a33bceb439e0bf7f903d064d8f
Thomas Langston <thomas.langston@gmail.com>
2014-04-05 12:03:40 -0400

    Make the API call to bitstamp

--- routes/order.js
+++ routes/order.js
@@ -1,7 +1,21 @@
+var request = require('request');
+
+var orders = [];
+var bitstampOrders = [];
+
 /* Get orders listing. */
 exports.list = function(req, res){
     //res.setHeader('Content-Type', 'application/json'); 
-    res.send({
+    //https://www.bitstamp.net/api/    
+    request('https://www.bitstamp.net/api/transactions/', function(error, response, body){
+        if(!error && response.statusCode == 200) {
+            //bitstampOrders = body; 
+            console.log(body);
+        }
+    });
+    //console.log(bitStampOrders);
+    res.send({ orders: orders });
+    /* res.send({
         orders: [
           {
             time: '10:57 am',
@@ -16,7 +30,8 @@ exports.list = function(req, res){
             exchange: 'bitstamp.net'    
           }
         ]
-    }); //TODO: Actually populate orders
+    }); */
+    //TODO: Actually populate orders
     //https://btc-e.com/api/documentation
     //https://www.bitstamp.net/api/
     //return last 10 orders across both apis listed above
