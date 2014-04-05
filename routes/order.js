6dc310f8f8756c2d91bdb92340d6420804a6de9c
Thomas Langston <thomas.langston@gmail.com>
2014-04-05 13:46:06 -0400

    broke the page on final combination, but I am out of time.

--- routes/order.js
+++ routes/order.js
@@ -11,15 +11,33 @@ exports.list = function(req, res){
 
     request('https://www.bitstamp.net/api/transactions/', function(error, response, body){
         if(!error && response.statusCode == 200) {
-            results.bitStampOrders = body;
+            results.bitStampOrders = JSON.parse(body);
         }
 
         request('https://btc-e.com/api/2/btc_usd/trades', function(error, response, body) {
             if(!error && response.statusCode == 200) {
-                results.btceOrders = body;
+                results.btceOrders = JSON.parse(body);
             }
-
-            console.log(results);
+            
+            var combinedOrders = _und.map(results.bitStampOrders, function(order){
+                return { 
+                    time: order.date, 
+                    quantity: order.amount, 
+                    price: order.price,
+                    exchange: 'bitstamp.net'
+                };
+            }) + _und.map(results.btceOrders, function(order){
+                return {
+                    time: order.date,
+                    quantity: order.amount,
+                    price: order.price,
+                    exchange: 'btc-e.com'
+                };
+            });
+            console.log(combinedOrders[0].time);
+            var sortedCombinedOrders = _und.sortBy(combinedOrders, function(order){ return -1 * order.date; });
+            results.orders = _und.first(sortedCombinedOrders, 10);
+            //console.log(results);
             res.send(results);
         });
     });
