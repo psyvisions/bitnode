6e6d49402e9418b94638795cc77e28a4e7a281bd
Thomas Langston <thomas.langston@gmail.com>
2014-04-05 10:27:34 -0400

    Note some of the requirements int the scaffolding

--- routes/order.js
+++ routes/order.js
@@ -1,4 +1,7 @@
 /* Get orders listing. */
 exports.list = function(req, res){
     res.send('response with orders'); //TODO: Actually populate orders
+    //https://btc-e.com/api/documentation
+    //https://www.bitstamp.net/api/
+    //return last 10 orders across both apis listed above
 };
