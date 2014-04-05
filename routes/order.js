6c795836d902b5115a711d0d3a657eb080e2b868
Thomas Langston <thomas.langston@gmail.com>
2014-04-05 12:52:57 -0400

    Chain my API calls so that the results are in the returned resource.

--- routes/order.js
+++ routes/order.js
@@ -1,45 +1,26 @@
 var request = require('request');
 var _und = require('underscore');
 
-var orders = [];
-var bitstampOrders = [];
-
 /* Get orders listing. */
 exports.list = function(req, res){
-    //res.setHeader('Content-Type', 'application/json'); 
-    //https://www.bitstamp.net/api/    
+    var results = {
+        orders: [],
+        bitStampOrders: [],
+        btceOrders: []
+    };
+
     request('https://www.bitstamp.net/api/transactions/', function(error, response, body){
         if(!error && response.statusCode == 200) {
-            //bitstampOrders = body; 
-            console.log(body);
+            results.bitStampOrders = body;
         }
-    });
 
-    request('https://btc-e.com/api/2/btc_usd/trades', function(error, response, body) {
-        if(!error && response.statusCode == 200) {
-            console.log(body);
-        }
+        request('https://btc-e.com/api/2/btc_usd/trades', function(error, response, body) {
+            if(!error && response.statusCode == 200) {
+                results.btceOrders = body;
+            }
+
+            console.log(results);
+            res.send(results);
+        });
     });
-    //console.log(bitStampOrders);
-    res.send({ orders: orders });
-    /* res.send({
-        orders: [
-          {
-            time: '10:57 am',
-            quantity: '5',
-            price: '900',
-            exchange: 'btc-e.com'  
-          },
-          {
-            time: '4:30pm',
-            quantity: '70',
-            price: ' 4.03',
-            exchange: 'bitstamp.net'    
-          }
-        ]
-    }); */
-    //TODO: Actually populate orders
-    //https://btc-e.com/api/documentation
-    //https://www.bitstamp.net/api/
-    //return last 10 orders across both apis listed above
 };
