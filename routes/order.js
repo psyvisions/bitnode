06ab22a5bf3ef34b902ebb9c9ba66f0b8ddc6326
Thomas Langston <thomas.langston@gmail.com>
2014-04-05 22:09:27 -0400

    Fix my silly mistake combining the arrays of objects

--- routes/order.js
+++ routes/order.js
@@ -19,14 +19,15 @@ exports.list = function(req, res){
                 results.btceOrders = JSON.parse(body);
             }
             
-            var combinedOrders = _und.map(results.bitStampOrders, function(order){
+            var mappedBitStampOrders = _und.map(results.bitStampOrders, function(order){
                 return { 
                     time: order.date, 
                     quantity: order.amount, 
                     price: order.price,
                     exchange: 'bitstamp.net'
                 };
-            }) + _und.map(results.btceOrders, function(order){
+            });
+            var mappedBtceOrders =  _und.map(results.btceOrders, function(order){
                 return {
                     time: order.date,
                     quantity: order.amount,
@@ -34,10 +35,10 @@ exports.list = function(req, res){
                     exchange: 'btc-e.com'
                 };
             });
-            console.log(combinedOrders[0].time);
+
+            var combinedOrders = _und.union(mappedBitStampOrders, mappedBtceOrders);
             var sortedCombinedOrders = _und.sortBy(combinedOrders, function(order){ return -1 * order.date; });
             results.orders = _und.first(sortedCombinedOrders, 10);
-            //console.log(results);
             res.send(results);
         });
     });
